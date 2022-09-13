c556cad258c7d9310806c439f5c808171c138152
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-09-13 16:27:51 -0700

    changes ?

--- NEW/src/routes/index@page.svelte
+++ NEW/src/routes/index@page.svelte
@@ -5,7 +5,104 @@
 	<title>SILICON FM</title>
 </svelte:head>
 
-<main />
+<main>
+	<div class="item flex">
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+		<h1>SEARCH</h1>
+	</div>
+</main>
 
 <style>
+	main {
+		justify-content: space-between;
+		max-height: calc(100vh - 100px);
+		max-width: calc(100vw - 40px);
+		padding: 16px 0;
+		background: #121212aa;
+		overflow: hidden;
+	}
+
+	h1 {
+		font-family: nb-television-2d, sans-serif;
+		cursor: pointer;
+	}
+
+	h1:hover {
+		font-family: nb-television-3d, sans-serif;
+		color: var(--accent);
+	}
+
+	.item {
+		width: 100%;
+	}
+
+	.item.flex {
+		display: flex;
+		width: 150vw;
+		gap: 0 20px;
+		flex-flow: row wrap;
+		padding: 0 40px;
+	}
 </style>
