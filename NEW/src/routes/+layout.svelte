625853233a7f754bac422513a972f64906a28adf
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-09-30 14:49:42 -0700

    hide mobile

--- NEW/src/routes/+layout.svelte
+++ NEW/src/routes/+layout.svelte
@@ -3,3 +3,23 @@
 </script>
 
 <slot />
+<div id="phoneBlock"><p class="sml">wip, use desktop</p></div>
+
+<style>
+	@media only screen and (max-width: 768px) {
+		#phoneBlock {
+			position: absolute;
+			top: 0;
+			left: 0;
+
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			height: 100vh;
+			width: 100vw;
+			background: var(--black);
+			z-index: 1000;
+		}
+	}
+</style>
